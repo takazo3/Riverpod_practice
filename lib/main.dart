4c3ebf042228d2ef39f39bbee5af0d0778b9428a
takazo <info@re-zero-jp.com>
2023-03-13 17:42:23 +0900

    Freezed学習

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_practice/Data/count_data.dart';
 import 'package:riverpod_practice/provider.dart';
 
 void main() {
@@ -49,34 +50,54 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
           children: <Widget>[
             Text(ref.watch(messageProvider)),
             Text(
-              ref.watch(countProvider).toString(),
+              ref.watch(countDataProvider).count.toString(),
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 FloatingActionButton(
-                  onPressed: () => ref
-                      .read(countProvider.state)
-                      .update((state) => state + 1),
-                  tooltip: 'Increment',
+                  onPressed: () {
+                    ref.read(countDataProvider);
+                    ref.watch(countDataProvider.notifier).update((state) =>
+                        state.copyWith(
+                            count: state.count + 1,
+                            countUp: state.countUp + 1));
+                  },
                   child: const Icon(CupertinoIcons.plus),
                 ),
                 FloatingActionButton(
-                  onPressed: () => ref
-                      .read(countProvider.state)
-                      .update((state) => state + 1),
-                  tooltip: 'Increment',
+                  onPressed: () {
+                    ref.read(countDataProvider);
+                    ref.watch(countDataProvider.notifier).update((state) =>
+                        state.copyWith(
+                            count: state.count - 1,
+                            countDown: state.countDown + 1));
+                  },
                   child: const Icon(CupertinoIcons.minus),
                 ),
               ],
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  ref.watch(countDataProvider).countUp.toString(),
+                ),
+                Text(
+                  ref.watch(countDataProvider).countDown.toString(),
+                ),
+              ],
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () =>
-            ref.read(countProvider.state).update((state) => state + 1),
-        tooltip: 'Increment',
+        onPressed: () {
+          ref.read(countDataProvider);
+          ref.watch(countDataProvider.notifier).update(
+              (state) => state.copyWith(count: 0, countUp: 0, countDown: 0));
+        },
         child: const Icon(Icons.refresh),
       ),
     ); // This trailing comma makes auto-formatting nicer for build methods.
--- lib/provider.dart
+++ lib/provider.dart
@@ -1,12 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'Data/count_data.dart';
 
 final titleProvider = Provider((ref) {
   return 'Riverpod';
 });
 
-final messageProvider = Provider((ref) =>
-  'You have pushed the button this many times:'
-);
+final messageProvider =
+    Provider((ref) => 'You have pushed the button this many times:');
 
-final countProvider = 
-    StateProvider<int>((ref) => 0);
+final countProvider = StateProvider<int>((ref) => 0);
+
+final countDataProvider = StateProvider<CountData>(
+  (ref) => const CountData(count: 0, countUp: 0, countDown: 0),
+);
